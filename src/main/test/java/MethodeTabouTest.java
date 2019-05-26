1593d55dd2f44200286003ef64a0b288c5697103
Antoine Ganne <antoine.ganne@hotmail.fr>
2019-05-26 17:49:00 +0200

    rapport

---
+++ src/main/test/java/MethodeTabouTest.java
@@ -0,0 +1,14 @@
+import com.polytech.landscape.ElementaryOperation;
+import com.polytech.landscape.Permutation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MethodeTabouTest {
+    @Test
+    @DisplayName("tests ")
+    void test(){
+
+    }
+}
